fc57127c64c0e1f3daf33306b6d01f03fe73a3bc
n-pretorious <pretoriousndungu@gmail.com>
2020-09-24 07:56:16 +0300

    Add user profile

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 main = Blueprint('main',__name__)
-# from . import views,errors
+
 try:
   from . import views,errors
 except:
--- app/main/views.py
+++ app/main/views.py
@@ -1,60 +1,95 @@
-from flask import render_template,request,redirect,url_for
+from flask import render_template, request, redirect, url_for, abort
 from . import main
-from ..requests import get_movies,get_movie,search_movie
-from .forms import ReviewForm
-from ..models import Review
+from ..requests import get_movies, get_movie, search_movie
+from .forms import ReviewForm, UpdateProfile
+from .. import db
+from ..models import Review, User
+from flask_login import login_required
 
 
 # Views
-@main.route('/')
+@main.route("/user/<uname>")
+def profile(uname):
+    user = User.query.filter_by(username=uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user=user)
+
+
+@main.route("/user/<uname>/update", methods=["GET", "POST"])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username=uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for(".profile", uname=user.username))
+
+    return render_template("profile/update.html", form=form)
+
+
+@main.route("/")
 def index():
-    '''
+    """
     View root page function that returns the index page and its data
-    '''
+    """
 
     # Getting popular movie
-    popular_movies = get_movies('popular')
-    upcoming_movie = get_movies('upcoming')
-    now_showing_movie = get_movies('now_playing')
+    popular_movies = get_movies("popular")
+    upcoming_movie = get_movies("upcoming")
+    now_showing_movie = get_movies("now_playing")
 
-    title = 'Home - Welcome to The best Movie Review Website Online'
+    title = "Home - Welcome to The best Movie Review Website Online"
 
-    search_movie = request.args.get('movie_query')
+    search_movie = request.args.get("movie_query")
 
     if search_movie:
-        return redirect(url_for('.search', movie_name=search_movie))
+        return redirect(url_for(".search", movie_name=search_movie))
     else:
-        return render_template('index.html',
-                               title=title,
-                               popular=popular_movies,
-                               upcoming=upcoming_movie,
-                               now_showing=now_showing_movie)
+        return render_template(
+            "index.html",
+            title=title,
+            popular=popular_movies,
+            upcoming=upcoming_movie,
+            now_showing=now_showing_movie,
+        )
 
 
-@main.route('/movie/<int:id>')
+@main.route("/movie/<int:id>")
 def movie(id):
-    '''
+    """
     View movie page function that returns the movie details page and its data
-    '''
+    """
     movie = get_movie(id)
-    title = f'{movie.title}'
+    title = f"{movie.title}"
 
-    return render_template('movie.html', title=title, movie=movie)
+    return render_template("movie.html", title=title, movie=movie)
 
 
-@main.route('/search/<movie_name>')
+@main.route("/search/<movie_name>")
 def search(movie_name):
-    '''
+    """
     View function to display the search results
-    '''
+    """
     movie_name_list = movie_name.split(" ")
     movie_name_format = "+".join(movie_name_list)
     searched_movies = search_movie(movie_name_format)
-    title = f'search results for {movie_name}'
-    return render_template('search.html', movies=searched_movies)
+    title = f"search results for {movie_name}"
+    return render_template("search.html", movies=searched_movies)
 
 
-@main.route('/movie/review/new/<int:id>', methods=['GET', 'POST'])
+@main.route("/movie/review/new/<int:id>", methods=["GET", "POST"])
+@login_required
 def new_review(id):
     form = ReviewForm()
     movie = get_movie(id)
@@ -64,10 +99,9 @@ def new_review(id):
         review = form.review.data
         new_review = Review(movie.id, title, movie.poster, review)
         new_review.save_review()
-        return redirect(url_for('.movie', id=movie.id))
+        return redirect(url_for(".movie", id=movie.id))
 
-    title = f'{movie.title} review'
-    return render_template('new_review.html',
-                           title=title,
-                           review_form=form,
-                           movie=movie)
+    title = f"{movie.title} review"
+    return render_template(
+        "new_review.html", title=title, review_form=form, movie=movie
+    )
